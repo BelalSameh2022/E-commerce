79190b63c4aef7a7600c404800565da898ba0a94
BelalSameh2022 <belal.ko3ip@gmail.com>
2024-08-10 02:43:57 +0300

    Enhance checkIfExists middleware

--- src/middlewares/checkIfExists.middleware.js
+++ src/middlewares/checkIfExists.middleware.js
@@ -5,39 +5,27 @@ import SubCategory from "../../database/models/subCategory.model.js";
 import Brand from "../../database/models/brand.model.js";
 import Product from "../../database/models/product.model.js";
 
-const checkIfUser = async (req, res, next) => {
-  const { email } = req.body;
-  const user = await User.findOne({ email: email?.toLowerCase() });
-  if (user) return next(new AppError("User already exists", 409));
-  next();
+const checkIfExists = (model, fieldName, fieldMessage) => {
+  return async (req, res, next) => {
+    const fieldValue = req.body[fieldName]?.toLowerCase();
+    const exists = await model.findOne({ [fieldName]: fieldValue });
+    if (exists) {
+      return next(new AppError(`${fieldMessage} already exists`, 409));
+    }
+    next();
+  };
 };
 
-const checkIfCategory = async (req, res, next) => {
-  const { name } = req.body;
-  const category = await Category.findOne({ name: name?.toLowerCase() });
-  if (category) return next(new AppError("Category already exists", 409));
-  next();
-};
+const checkIfUser = checkIfExists(User, "email", "User");
+const checkIfCategory = checkIfExists(Category, "name", "Category");
+const checkIfSubCategory = checkIfExists(SubCategory, "name", "SubCategory");
+const checkIfBrand = checkIfExists(Brand, "name", "Brand");
+const checkIfProduct = checkIfExists(Product, "name", "Product");
 
-const checkIfSubCategory = async (req, res, next) => {
-  const { name } = req.body;
-  const subCategory = await SubCategory.findOne({ name: name?.toLowerCase() });
-  if (subCategory) return next(new AppError("SubCategory already exists", 409));
-  next();
+export {
+  checkIfUser,
+  checkIfCategory,
+  checkIfSubCategory,
+  checkIfBrand,
+  checkIfProduct,
 };
-
-const checkIfBrand = async (req, res, next) => {
-  const { name } = req.body;
-  const brand = await Brand.findOne({ name: name?.toLowerCase() });
-  if (brand) return next(new AppError("Brand already exists", 409));
-  next();
-};
-
-const checkIfProduct = async (req, res, next) => {
-  const { name } = req.body;
-  const product = await Product.findOne({ name: name?.toLowerCase() });
-  if (product) return next(new AppError("Product already exists", 409));
-  next();
-};
-
-export { checkIfUser, checkIfCategory, checkIfSubCategory, checkIfBrand, checkIfProduct };
